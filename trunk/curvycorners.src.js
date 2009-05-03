da041d6d7a23e592bce8ed77a7d8cfd26df313ea
c.1@smithies.org <c.1@smithies.org@0eb09534-ac23-11dd-9a14-c1645b63b0c6>
2009-05-03 18:30:18 +0000

    Warn about unsupported feature: unequal top/bottom corner radii Simplify some expressions

--- trunk/curvycorners.src.js
+++ trunk/curvycorners.src.js
@@ -699,15 +699,10 @@ function curvyObject() {
 
         if (this.backgroundImage) switch(cc) {
           case "tr":
-            if (curvyBrowser.quirksMode) {
-              //pixelBar.style.backgroundPosition = (this.backgroundPosX - Math.abs(0 - this.borderWidthL + this.boxWidth - specRadius + pixelBarLeft)) + "px " + (this.backgroundPosY - Math.abs(specRadius - pixelBarHeight - pixelBarTop - this.borderWidth)) + "px";
-              pixelBar.style.backgroundPosition = (this.backgroundPosX + this.borderWidthL + specRadius - this.boxWidth - pixelBarLeft) + "px " + (this.backgroundPosY + pixelBarHeight + pixelBarTop + this.borderWidth - specRadius) + "px";
-            } else {
-              pixelBar.style.backgroundPosition = (this.backgroundPosX - Math.abs(this.borderWidthR - this.borderWidthL + this.boxWidth - specRadius + this.borderWidthR + pixelBarLeft)) + "px " + (this.backgroundPosY - Math.abs(specRadius - pixelBarHeight - pixelBarTop - this.borderWidth)) + "px";
-            }
+            pixelBar.style.backgroundPosition = (this.backgroundPosX + this.borderWidthL + specRadius - this.boxWidth - pixelBarLeft) + "px " + (this.backgroundPosY + pixelBarHeight + pixelBarTop + this.borderWidth - specRadius) + "px";
           break;
           case "tl":
-            pixelBar.style.backgroundPosition = parseInt(this.backgroundPosX - Math.abs((specRadius - pixelBarLeft -1)  - this.borderWidthL)) + "px " + parseInt(this.backgroundPosY - Math.abs(specRadius - pixelBarHeight - pixelBarTop - this.borderWidth)) + "px";
+            pixelBar.style.backgroundPosition = (this.backgroundPosX - Math.abs((specRadius - pixelBarLeft - 1)  - this.borderWidthL)) + "px " + (this.backgroundPosY - Math.abs(specRadius - pixelBarHeight - pixelBarTop - this.borderWidth)) + "px";
           break;
           case "bl":
             if (curvyBrowser.quirksMode) {
@@ -721,7 +716,7 @@ function curvyObject() {
 //              pixelBar.style.backgroundPosition = (this.backgroundPosX - Math.abs(1 + this.leftPadding - this.borderWidthL + this.boxWidth - specRadius + pixelBarLeft)) + "px " + (this.backgroundPosY - Math.abs(this.boxHeight + (this.borderWidth - this.topPadding - 1) - specRadius + pixelBarTop)) + "px";
                 pixelBar.style.backgroundPosition = (this.backgroundPosX - 1 + this.borderWidthL - clientWidth + specRadius - pixelBarLeft) + "px " + (this.backgroundPosY - clientHeight - this.borderWidth + pixelBarTop + specRadius) + "px";
             } else {
-              pixelBar.style.backgroundPosition = (this.backgroundPosX - (this.borderWidthR - this.borderWidthL + this.boxWidth - specRadius + this.borderWidthR + pixelBarLeft)) + "px " + (this.backgroundPosY - Math.abs((this.boxHeight + (this.borderWidth + this.topPadding + this.bottomPadding) - specRadius + pixelBarTop))) + "px";
+              pixelBar.style.backgroundPosition = (this.backgroundPosX - (this.borderWidthR - this.borderWidthL + this.boxWidth - specRadius + this.borderWidthR + pixelBarLeft)) + "px " + (this.backgroundPosY - this.boxHeight - this.borderWidth - this.topPadding - this.bottomPadding + specRadius - pixelBarTop) + "px";
             }
           //break;
         }
@@ -762,6 +757,8 @@ function curvyObject() {
       if (typeof z === 'function') continue; // for prototype, mootools frameworks
       if (!this.spec.get(z + 'R')) continue; // no need if no corners
       if (radiusDiff[z]) {
+        // check unsupported feature and warn if necessary
+        if (this.backgroundImage) curvyCorners.alert(this.errmsg('Not supported: unequal top/bottom radii with background image')); 
         // Get the type of corner that is the smaller one
         var smallerCornerType = (this.spec[z + "lR"] < this.spec[z + "rR"]) ? z + "l" : z + "r";
 
